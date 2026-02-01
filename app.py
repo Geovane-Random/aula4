5c82c39bd2f0b056e4f351f87de1ce0968c10f73
Geovane-Random <geovane.dnk@gmail.com>
2026-02-01 14:28:46 -0300

    local csv

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 # --- Carregamento dos dados ---
 @st.cache_data
 def load_data():
-    return pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
+    return pd.read_csv("dados-imersao-final.csv")
 
 df = load_data()
 
